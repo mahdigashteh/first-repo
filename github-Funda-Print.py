bd4a15a7862b8939c75630eeb0abaf2b6fe198b3
mahdigashteh <154457689+mahdigashteh@users.noreply.github.com>
2023-12-26 16:51:20 +0330

    Add files via upload

---
+++ github-Funda-Print.py
@@ -0,0 +1,15 @@
+
+
+# The default value of the end parameter of the built-in print function is \n
+# it means : print kon va be khate jadid boro
+print("hi")
+# the below command will go to 2 new line ( first for print itself , second for \n)
+print("\n")
+print("bye")
+
+
+# be khate jadid naro
+i = 2
+j = 3
+print(i*j, end="")
+print("don't go to new line")
